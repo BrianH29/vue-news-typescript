9c71d0aef43af1d98b0f8d831b3cc151bdd5fd88
Brian Hwang <brianhwang29@gmail.com>
2022-01-03 06:39:19 +0900

    initial commit

---
+++ src/views/ListView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <list-item></list-item>
+  </div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue';
+import { fetchNews } from '@/api';
+
+export default {
+  components: {
+    ListItem,
+  },
+
+  data() {
+    return {
+      newsItems: [],
+    };
+  },
+
+  created() {
+    this.fetchNewsItems();
+  },
+
+  methods: {
+    async fetchNewsItems() {
+      const response = await fetchNews();
+      this.newsItems = response.data;
+    },
+  },
+};
+</script>
+
+<style></style>
